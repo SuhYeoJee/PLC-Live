db8c47d7157a181ba3596e55c6cd3dbbb7de6f08
SuhYeoJee <suhyeojee@gmail.com>
2024-10-08 21:59:42 +0900

    알람, 그래프 처리 초안

--- src/model.py
+++ src/model.py
@@ -3,6 +3,7 @@ if __debug__:
     sys.path.append(r"X:\Github\PLC-Live")
 # -------------------------------------------------------------------------------------------
 from src.LS_PLC import LS_plc
+from src.module.plcl_utils import get_now_str
 from src.state import connect_wait, start_wait, exit_wait
 # --------------------------
 # from igzg.utils import get_config
@@ -16,6 +17,8 @@ class Model():
         self._init_state()
         # self.graph_data = []
         # self.alarm_data = self._init_alarm()
+        self.alarms = {} # {label:'on', label:'off'}
+        self.graph_value = None
 
     def _init_state(self)->None:
         self.c_w = connect_wait()
@@ -26,11 +29,7 @@ class Model():
         self.s_w.next_state = self.e_w
         self.e_w.next_state = self.s_w
         # --------------------------
-        self.state = self.e_w # 초기상태
-
-    # def _init_alarm(self)->dict:
-    #     alarm_data = {k:'0' for k,v in self.e_w.addrs["PLC_ADDR"]["ALARM"].items()}
-    #     return alarm_data
+        self.state = self.c_w # 초기상태
 
     # [PLC] -------------------------------------------------------------------------------------------
     def _get_update_data(self)->dict:
@@ -63,15 +62,56 @@ class Model():
         2. state 전환
         '''
         self.state.before_worker_tick()
+
         update_data = self._get_update_data() # plc 데이터 읽기
-        # update_data 기반 처리
-        # 알람
-        # 그래프
+        alarm_data = self.update_alarm(update_data)
+        is_graph_update = self.update_graph(update_data)
+
         is_next = self.state._is_next(update_data[self.state.key]) # 읽은 항목에서 state체크
         if is_next: # state 넘어가기
             self._change_mode()
         self.state.after_worker_tick(update_data=update_data)
-        return update_data
+        return update_data,alarm_data,is_graph_update
+    
+
+    def update_alarm(self,update_data:dict)->dict:
+        result = {}
+        if 'alarm' not in update_data.keys():
+            return result
+        
+        # 알람 on off 판단 (함수 분리 필)
+        alarms = {}
+        for label,plc_addr in self.state.addrs['alarm'].items():
+            addr, _, option = plc_addr.partition('#')
+            value = update_data['alarm'][label]
+            if option[2] == "a":
+                alarms[label] = 'on' if value == 1 else 'off'
+            elif option[2] == "A":
+                alarms[label] = 'off' if value == 1 else 'on'
+        
+        # 기존 상태와 비교
+        for label,value in alarms.items():
+            try:
+                if value != self.alarms[label]:
+                    result[label] = [value,get_now_str()]
+            except IndexError:
+                result[label] = [value,get_now_str()]
+        else:
+            self.alarms = alarms
+
+        return result #{arr_label:['on',timestr]}
+
+    def update_graph(self,update_data)->bool:
+        if 'AUTOMATIC' not in update_data.keys():
+            return False
+        
+        # 그래프 갱신 판단
+        graph_value = self.state.addrs['AUTOMATIC']['graph_addr']
+        if graph_value != self.graph_value:
+            self.graph_value = graph_value
+            return True
+        
+        return False 
 
 
 # ===========================================================================================
