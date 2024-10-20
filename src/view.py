87986ff324bbd4e2f01232b3f9d4db49a28ee0dc
SuhYeoJee <suhyeojee@gmail.com>
2024-10-20 18:04:23 +0900

    view] 슬라이더 일부 구현

--- src/view.py
+++ src/view.py
@@ -47,18 +47,19 @@ class View(QMainWindow, uic.loadUiType("./ui/mainwindow.ui")[0]) :
         self.ALARM_TABLE.setColumnWidth(0, 200)
     
     def init_graph(self):
+        self.graph_width = 7
         self.graph_widget = pg.PlotWidget()
         self.graph_widget.setBackground('w')
         QVBoxLayout(self.graph_frame).addWidget(self.graph_widget)
 
     # [TABLE 라벨 매핑] ===========================================================================================
-    def set_text_PROGRAM_TABLE(self, key, val)->None:
+    def _set_text_PROGRAM_TABLE(self, key, val)->None:
         i = int(''.join(filter(str.isdigit, key)))-1
         for j,item in enumerate(["STEPDIMENSION","CHARGEDIMENSION","FWDTIME","SELECTCAR","OSCCOUNT","BWDTIME","PRESSPOSITION","FINALPRESSURE","SELECTDIA"]):
             if not (item in key): continue
             self.PROGRAM_TABLE.setItem(i, j, QTableWidgetItem(val))
     # --------------------------
-    def set_text_PROGRAM_LIST_TABLE(self, key, val)->None:
+    def _set_text_PROGRAM_LIST_TABLE(self, key, val)->None:
         i = int(''.join(filter(str.isdigit, key)))-1
         j = (i//10)*2+1
         i = i%10
@@ -68,13 +69,13 @@ class View(QMainWindow, uic.loadUiType("./ui/mainwindow.ui")[0]) :
             if not (item in key): continue
             self.PROGRAM_LIST_TABLE.setItem(i, j, QTableWidgetItem(val))
     # --------------------------
-    def set_text_PROGRAM_VIEW_TABLE(self, key, val)->None:
+    def _set_text_PROGRAM_VIEW_TABLE(self, key, val)->None:
         i = int(''.join(filter(str.isdigit, key)))-1
         for j,item in enumerate(["STEPDIMENSION","CHARGEDIMENSION","FWDTIME","SELECTCAR","OSCCOUNT","BWDTIME","PRESSPOSITION","FINALPRESSURE","SELECTDIA"]):
             if not (item in key): continue
             self.PROGRAM_VIEW_TABLE.setItem(i, j, QTableWidgetItem(val))
     # --------------------------
-    def set_text_ALARM_TABLE(self,key,val)->None:
+    def _set_text_ALARM_TABLE(self,key,val)->None:
         '''
         key = 'ALARM_EMERGENCY_STOP'
         val = ['on', '2024-10-20 15:44:13']
@@ -93,11 +94,11 @@ class View(QMainWindow, uic.loadUiType("./ui/mainwindow.ui")[0]) :
     def set_text(self,update_data:dict)->None:
         for k,v in update_data.items():
             if 'PROGRAM_TABLE' in k: # 테이블에 값 표시
-                self.set_text_PROGRAM_TABLE(k,v)
+                self._set_text_PROGRAM_TABLE(k,v)
             elif 'PROGRAM_LIST_TABLE' in k:
-                self.set_text_PROGRAM_LIST_TABLE(k,v)
+                self._set_text_PROGRAM_LIST_TABLE(k,v)
             elif 'PROGRAM_VIEW_TABLE' in k:
-                self.set_text_PROGRAM_VIEW_TABLE(k,v)
+                self._set_text_PROGRAM_VIEW_TABLE(k,v)
             else: # line edit에 값 표시
                 try:
                     lineedit = self.findChild(QLineEdit, k, Qt.FindChildrenRecursively)
@@ -110,22 +111,44 @@ class View(QMainWindow, uic.loadUiType("./ui/mainwindow.ui")[0]) :
                     ...
                     #print_error_box(e,k,v)
     # --------------------------
-    def clear_table(self,table)->None:
+    def _clear_table(self,table)->None:
         for row in range(table.rowCount()):
             for col in range(table.columnCount()):
                 table.setItem(row, col, QTableWidgetItem(''))
     # --------------------------
-    def clear_graph(self)->None:
+    def _clear_graph(self)->None:
         self.graph_widget.clear()
     # --------------------------
     def clear_window(self)->None:
-        self.clear_table(self.PROGRAM_TABLE)
-        self.clear_table(self.PROGRAM_LIST_TABLE)
-        self.clear_table(self.PROGRAM_VIEW_TABLE)
-        self.clear_table(self.ALARM_TABLE)
-        self.clear_graph()
+        self._clear_table(self.PROGRAM_TABLE)
+        self._clear_table(self.PROGRAM_LIST_TABLE)
+        self._clear_table(self.PROGRAM_VIEW_TABLE)
+        self._clear_table(self.ALARM_TABLE)
+        self._clear_graph()
         for line_edit in self.findChildren(QLineEdit):
             line_edit.clear()
+    # --------------------------
+    def set_alarm(self,alarm_data:dict)->None:
+        for k,v in alarm_data.items():
+            self._set_text_ALARM_TABLE(k,v)     
+    # --------------------------
+    def update_graph(self,graph_points:list):
+        '''
+        리스트 값을 그래프에 표기
+        슬라이더 세팅
+        '''
+        idx = len(graph_points)-1
+        self.horizontalSlider.setMinimum(0)
+        self.horizontalSlider.setMaximum(idx)
+        self.horizontalSlider.setValue(idx)
+
+        self.graph_widget.plot(graph_points, pen=pg.mkPen('g', width=self.graph_width))
+        try: #기존 세로선 삭제
+            self.graph_widget.removeItem(self.vertical_line)
+        except: ...
+        self.vertical_line = pg.InfiniteLine(pos=self.horizontalSlider.value(), angle=90, pen=pg.mkPen('r', width=2))
+        self.graph_widget.addItem(self.vertical_line)
+        self.horizontalSlider.valueChanged.connect(self._slider_update_vertical_line)
 
     # [external call] ===========================================================================================
     def open_file_dialog(self)->str:
@@ -146,42 +169,24 @@ class View(QMainWindow, uic.loadUiType("./ui/mainwindow.ui")[0]) :
         alert.setStandardButtons(QMessageBox.Ok)  # 확인 버튼 설정
         alert.exec_()  # 알림창 실행
     # --------------------------
-    def change_window(self,title): 
+    def change_window_title(self,title): 
         self.setWindowTitle(f"PressMonitor: {title}")
-    # --------------------------
-    def set_alarm(self,alarm_data:dict)->None:
-        for k,v in alarm_data.items():
-            self.set_text_ALARM_TABLE(k,v)     
 
 # ===========================================================================================
-    # def update_graph(self,graph_points):
-    #     self.graph_width = 7
 
-    #     idx = len(graph_points)-1
-    #     self.horizontalSlider.setMinimum(0)
-    #     self.horizontalSlider.setMaximum(idx)
-    #     self.horizontalSlider.setValue(idx)
-
-    #     self.graph_widget.plot(graph_points, pen=pg.mkPen('g', width=self.graph_width))
-
-    #     try:
-    #         self.graph_widget.removeItem(self.vertical_line)
-    #     except: ...
-    #     self.vertical_line = pg.InfiniteLine(pos=self.horizontalSlider.value(), angle=90, pen=pg.mkPen('r', width=2))
-    #     self.graph_widget.addItem(self.vertical_line)
-    #     self.horizontalSlider.valueChanged.connect(self.update_vertical_line)
-
-    # def update_vertical_line(self, value):
-    #     '''
-    #     슬라이더 이동시 동작
-    #     - 세로선 갱신
-    #     - 그래프 위치 갱신
-    #     + set_text 갱신(view에서)
-    #     '''
-    #     self.vertical_line.setPos(value)
-    #     self.graph_widget.setXRange(value - self.graph_width/2, value + self.graph_width/2)
-    #     # 여기 VALUE값을 IDX로해서 테이블 갱신필.
-    #     # self.c.set_load_data(value)
+    def _slider_update_vertical_line(self, value):
+        '''
+        슬라이더 이동시 동작
+        - 세로선 갱신
+        - 그래프 위치 갱신
+        + set_text 갱신(view에서)
+        '''
+        self.vertical_line.setPos(value)
+        self.graph_widget.setXRange(value - self.graph_width/2, value + self.graph_width/2)
+        # 여기 VALUE값을 IDX로해서 테이블 갱신필.
+        # self.c.set_load_data(value)
+        # 여기서 테이블 값을 가져오는 방법...
+        
 
 # ===========================================================================================
 if __name__ == "__main__":
