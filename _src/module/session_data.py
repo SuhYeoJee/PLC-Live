a251e5df7ae778b98fc8647b7d9937df4b7a00a8
SuhYeoJee <72551449+SuhYeoJee@users.noreply.github.com>
2024-12-10 11:35:35 +0900

    재설계 시작

--- _src/module/session_data.py
+++ _src/module/session_data.py
@@ -11,25 +11,24 @@ from pprint import pprint
 # ===========================================================================================
 
 '''
-엑셀 형식 {시트명:[{열이름:값,}]} -> 갱신시 새 딕셔너리를 리스트에 추가
-
+    수정불필요
 '''
 
 class SessionData:
     def __init__(self,file_name:str=None):
         if file_name: #load data
-            '''excel 파일에서 데이터 읽기'''
             self.file_name = file_name
-            self._read_data_from_excel(file_name)
+            self.read_data_from_excel(file_name)
             self.is_new:bool = False
         else: #new data
-            '''json 파일에서 데이터형식 읽기'''
             self.file_name = get_now_str("./result/%Y-%m-%d_%H-%M-%S.xlsx")
             self.is_new:bool = True
-            self.data = self._get_data_form()
+            # 엑셀 형식 {시트명:[{열이름:값,}]} #갱신시 새 딕셔너리를 리스트에 추가
+            self.data = self.get_data_form()
             self.update_idx_sheet()
+
     # --------------------------
-    def _get_data_form(self):
+    def get_data_form(self):
         '''
         json.PLC_ADDR 항목 자동으로 읽어옴
         DATASET 이름을 시트명으로, 주소명을 열제목으로 사용
@@ -57,21 +56,30 @@ class SessionData:
     def save_data_to_excel(self):
         '''
         sessionData를 엑셀로 저장 
-        temp.xlsx 생성 후 스왑
         '''
         temp_file_name = "temp.xlsx"
         try:
             if not path.exists('result'): makedirs('result')
             with pd.ExcelWriter(temp_file_name) as writer:
                 for sheet_name, sheet_data in self.data.items():
+                    # df = pd.DataFrame(sheet_data[1:]) # (초기 빈값 제외)
                     df = pd.DataFrame(sheet_data)
                     df.to_excel(writer, sheet_name=sheet_name, index=False)
             shutil.copy(temp_file_name, self.file_name)
         finally:
             if path.exists(temp_file_name):
                 remove(temp_file_name)
+    
+    def update_idx_sheet(self):
+        '''
+        호출된 순간의 각 시트의 line 수를 idx 시트에 기록
+        '''
+        idx_dict = {k:len(self.data[k]) for k in self.data.keys()}
+        self.update_data('idx',idx_dict)
+        self.save_data_to_excel()
+    
     # --------------------------
-    def _read_data_from_excel(self,file_name):
+    def read_data_from_excel(self,file_name):
         '''
         엑셀 값을 self.data형식에 맞게 읽기 
         '''
@@ -98,5 +106,6 @@ if __name__=="__main__":
     s = SessionData()
     pprint(s.data)
     s.update_data("AUTOMATIC",{"AUTOMATIC_PRGNO":"13","AUTOMATIC_PRGNAME":"dtf"})
+    s.update_idx_sheet()
     pprint(s.data)
     s.save_data_to_excel()
